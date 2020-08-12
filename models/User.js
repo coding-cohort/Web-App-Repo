bce5cfa13173194953df481dbf99f88bc742730a
VijayKumarKTG <vijaykumarktg18@gmail.com>
2020-08-12 11:21:43 +0630

    Add full authentication sign up, login, logout, forget and reset password.

--- models/User.js
+++ models/User.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose'),
   passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
+  username: { type: String, required: true },
   password: String,
   surname: String,
   email: { type: String, unique: true, required: true },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
 });
 
-UserSchema.plugin(passportLocalMongoose);
+UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 
 module.exports = mongoose.model('User', UserSchema);
