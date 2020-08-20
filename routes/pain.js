8d9a8db4afa83178a69419308675dce0a322e820
VijayKumarKTG <vijaykumarktg18@gmail.com>
2020-08-20 11:55:54 +0630

    Add pain data retriving api

--- routes/pain.js
+++ routes/pain.js
@@ -58,6 +58,35 @@ router.post('/pain', (req, res) => {
     .catch((err) => console.log(err));
 });
 
+// API routes for getting pain data
+router.get('/api/pain/:userId/:timeframe?', (req, res) => {
+  let userId = req.params.userId;
+  let timeframe = req.params.timeframe;
+  Pain.findOne({ userId }, (err, data) => {
+    if (err) {
+      return res.json({ error: err.message });
+    } else {
+      if (timeframe) {
+        if (
+          timeframe === 'daily' ||
+          timeframe === 'weekly' ||
+          timeframe === 'monthly'
+        ) {
+          let obj = {};
+          obj[timeframe] = data[timeframe];
+          return res.json(obj);
+        } else {
+          return res.json({
+            error: `${timeframe} is not a valid timeframe. You can use daily, weekly and montly instead.`,
+          });
+        }
+      } else {
+        return res.json(data);
+      }
+    }
+  });
+});
+
 const painLevels = (painNum, pain, timeframe, length) => {
   let time = pain[timeframe];
   if (time.length === 0) {
